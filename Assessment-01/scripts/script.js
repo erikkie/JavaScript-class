4e534de5f90b92a7eb1cd70d726e1dd12a2a44cd
Erika Howell <98297857+erikkie@users.noreply.github.com>
2022-01-24 22:59:31 -0600

    Add files via upload

---
+++ Assessment-01/scripts/script.js
@@ -0,0 +1,20 @@
+function gameRules2(){
+    document.getElementById("games").innerHTML = "It's the cat! or is it?";
+}
+
+function sizeSmall(){
+    document.getElementById("house").style.width = "20px" ;
+}
+
+function sizeBig(){
+    document.getElementById("house").style.width = "200px" ;
+}
+
+function colorChange(){
+    document.getElementById("games").style.color = "#000" ;
+}
+
+function colorChange1(){
+    document.getElementById("games").style.color = "#ededed" ;
+}
+
