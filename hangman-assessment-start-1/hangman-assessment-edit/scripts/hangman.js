0b8551dcd42162914374785e410689c2bc5aa06d
Erika Howell <ehowell@students.mchenry.edu>
2022-02-16 22:34:17 -0600

    feb16

--- hangman-assessment-start-1/hangman-assessment-edit/scripts/hangman.js
+++ hangman-assessment-start-1/hangman-assessment-edit/scripts/hangman.js
@@ -9,6 +9,11 @@ let letters = answer.split('');
 let attemptsLeft = 6;
 let output = '';
 let userLetter = '';
+let found = false;
+let incorrect = '';
+
+const photos = ["images/02.png","images/03.png", "images/04.png", "images/05.png", "images/06.png", "images/07.png"]
+
 // game setup works fine --steffen
 function setup() {
     alert(answer);
@@ -21,7 +26,6 @@ function setup() {
 
 
 
-
 // Issue below here
 // noticed if we uncomment below it reveals all letter for the hangman
 
@@ -31,27 +35,69 @@ document.getElementById("submit").addEventListener("click", function(event){
     output = '';
     userLetter = document.getElementById("guess").value;
     document.getElementById("guess").value = ''; //would this reset value to null
-
+    found = false;
     for (let c = 0; c < answer.length; c++) {
         //alert(letters[c]);
-        found = false;
+
         if (userLetter.toUpperCase() == letters[c]) {
+            found = true;
             display[c] = userLetter.toUpperCase();
             win--;
-            found = true;         
+                                          
         }
         output = output + display[c] + ' ';
     }
-    
-    if (found == false){
-        
-        attemptsLeft --;
-        listA = []         
+
+    if (found == false){        
+        listB = incorrect += userLetter + ' , ';
+        document.getElementById("guessed").innerHTML = listB;
+        attemptsLeft --;  
+         
+        /*listA = []         
         listA = list(map(userLetter).strip().split(','))
-        print(listA)
+        print(listA)*/
+    } 
+    if (attemptsLeft < 6){
+        document.getElementById("hangman1").style.display = "none";
+        document.getElementById("hangman2").style.display = "block";
         
     }
-   
+    if (attemptsLeft < 5){
+        document.getElementById("hangman2").style.display = "none";
+        document.getElementById("hangman3").style.display = "block";
+        
+    }
+    if (attemptsLeft < 4){
+        document.getElementById("hangman3").style.display = "none";
+        document.getElementById("hangman4").style.display = "block";
+        
+    }
+    if (attemptsLeft < 3){
+        document.getElementById("hangman4").style.display = "none";
+        document.getElementById("hangman5").style.display = "block";
+        
+    }
+    if (attemptsLeft < 2){
+        document.getElementById("hangman5").style.display = "none";
+        document.getElementById("hangman6").style.display = "block";
+        
+    }
+    if (attemptsLeft < 2){
+        document.getElementById("hangman6").style.display = "none";
+        document.getElementById("hangman7").style.display = "block";
+        
+    }
+    
+    /*if (found == false){
+        
+    }
+    if (found == false){
+      
+    }
+     
+    if (found == false){
+       
+    }*/
 
     if (win < 1) {
          document.getElementById("guesses").innerHTML = 'YOU WIN!!!';
@@ -66,7 +112,5 @@ document.getElementById("submit").addEventListener("click", function(event){
     
 
 });
-
-
 
 //scirpt connected fine somereason the button and the script are not registering.*/
