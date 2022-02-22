74ef9573c24da55d2df6d77ec21bbbb1cda9baec
Erika Howell <ehowell@students.mchenry.edu>
2022-02-21 23:27:51 -0600

    feeb21

---
+++ hotncold/guess.js
@@ -0,0 +1,20 @@
+if (guess < (jackPot - 100)) {
+    result = "Freezing";
+}
+else if (guess > (jackpot + 100)){
+    result = "Freezing";
+}
+else if (guess < (jackPot - 50)){
+    result = "Colder";
+}
+else if (guess < (jackPot + 50)){
+    result = "Colder";
+}
+else if (guess < (jackPot - 25)){
+    result = "Warmer";
+}
+else if (guess < (jackPot - 25)){
+    result = "Warmer";
+}
+
+}
